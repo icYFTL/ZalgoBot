99c198206be12f6ca821616fa9634dcd44bfafdd
icYFTL <savap0@yandex.ru>
2019-04-18 22:48:55 +0300

    Async_Beta

--- source/ApiWorker.py
+++ source/ApiWorker.py
@@ -14,18 +14,19 @@ class ApiWorker:
         for admin in Config.admins:
             self.botapi.message_send('Скрипт начал работу.', admin, False)
 
-    def get_message(self):
+    async def get_message(self):
+
         data = self.botapi.message_handler()
 
-        if not iniWorker.user_exists(data[1]):
-            iniWorker.createConfig(data[1])
+        if not await iniWorker.user_exists(data[1]):
+            await iniWorker.createConfig(data[1])
 
         if '/' in str(data[0]):
             CH = CommandsHandler(data[1])
-            CH.identify_comma(data[0])
+            await CH.identify_comma(data[0])
             return
 
         if data[0]:
-            self.botapi.message_send(data[0], data[1], True)
+            await self.botapi.message_send(data[0], data[1], True)
         else:
-            self.botapi.message_send('Гони текст, а не вот это все.', data[1], False)
+            await self.botapi.message_send('Гони текст, а не вот это все.', data[1], False)
