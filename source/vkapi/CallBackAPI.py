8571c78a820e3c9cd9254d5cffed4ebde37dd68b
icyftl <savap0@yandex.ru>
2019-07-24 02:56:18 +0300

    Fixed access token getting

--- source/vkapi/CallBackAPI.py
+++ source/vkapi/CallBackAPI.py
@@ -4,8 +4,8 @@ import logging
 import requests
 from flask import Flask, request, redirect
 
-from source.databases.InternalBD import InternalBD
 from Static.StaticData import StaticData
+from source.databases.InternalBD import InternalBD
 
 m_thread = Flask(__name__)
 
@@ -15,11 +15,14 @@ log.setLevel(logging.ERROR)
 
 @m_thread.route('/', methods=['GET'])
 def get_access():
-    code = request.args['code']
-    access = json.loads(requests.get(
-        "https://oauth.vk.com/access_token?client_id=6949573&client_secret=yTy9ne0P6B0MmzZLDLPr&redirect_uri=http://icyftl.ru/ZalgoBot/GetAccess.php&code=" + code).text)
-    InternalBD.add_token(user_id=access['user_id'], token=access['access_token'])
-    return redirect("https://vk.com/im?sel=-181269537", code=302)
+    try:
+        code = request.args['code']
+        access = json.loads(requests.get(
+            "https://oauth.vk.com/access_token?client_id=6949573&client_secret=yTy9ne0P6B0MmzZLDLPr&redirect_uri=http://http://195.133.144.12/&code=" + code).text)
+        InternalBD.add_token(user_id=access['user_id'], token=access['access_token'])
+        return redirect("https://vk.com/im?sel=-181269537", code=302)
+    except:
+        return "Welcome to ZalgoBot server!"
 
 
 @m_thread.route('/', methods=['POST'])
