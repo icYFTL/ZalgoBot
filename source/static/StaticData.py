f9a7f87446c809e52591d1c469ba30265f5df1b1
icYFTL <savap0@yandex.ru>
2019-11-23 18:12:05 +0300

    Fixes

--- source/static/StaticData.py
+++ source/static/StaticData.py
@@ -3,7 +3,7 @@ import threading
 
 class StaticData:
     name = 'ZalgoBot'
-    version = '1.9.3'
+    version = '1.9.5'
     author = 'icYFTL'
     stack_messages = []
     new_message_trigger = threading.Event()
