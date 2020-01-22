d72e8151a98a90baa7af059c4b87ab00e2419db8
icYFTL <savap0@yandex.ru>
2020-01-22 04:11:29 +0300

    VK access token getting improvements

--- source/static/StaticData.py
+++ source/static/StaticData.py
@@ -3,7 +3,7 @@ import threading
 
 class StaticData:
     name = 'ZalgoBot'
-    version = '2.0.4'
+    version = '2.0.5'
     author = 'icYFTL'
     stack_messages = []
     new_message_trigger = threading.Event()
